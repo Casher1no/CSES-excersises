daace0acb6fad66fc4fa82950a313df9cd4b3e1c
Maikls <maikls.zalkalns@gmail.com>
2026-08-15 12:55:19 +0300

    fixes

--- template.cpp
+++ template.cpp
@@ -1,14 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    ll n;
+    int n;
     cin >> n;
-    vector<ll> a(n);
+    vector<int> a(n);
     for (auto &x : a) cin >> x;
 
     cout << n << " ";
