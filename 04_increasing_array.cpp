4f4a5dd9380910cd320a2b1d5e33a40ec7b1b15e
Maikls <maikls.zalkalns@gmail.com>
2026-08-16 12:13:36 +0300

    Completed increasing array

--- 04_increasing_array.cpp
+++ 04_increasing_array.cpp
@@ -7,5 +7,17 @@ int main() {
     vector<int> a(n);
     for (auto &x : a) cin >> x;
 
-    cout << n << " ";
+    long long output = 0;
+    for (int i = 1; i < n; i++) {
+        int previous = a[i - 1];
+        int current = a[i];
+
+        while(previous > current) {
+            current++;
+            output++;
+        }
+        a[i] = current;
+    }
+
+    cout << output;
 }
